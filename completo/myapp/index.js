16609e1490bad64513485ad125b34c03c0fd2c9f
Walter Dal Mut <walter.dalmut@gmail.com>
2019-12-04 17:46:12 +0100

    Kustomize + Skaffold example

---
+++ completo/myapp/index.js
@@ -0,0 +1,7 @@
+const express = require('express')
+const app = express()
+const port = 4000
+
+app.get('/', (req, res) => res.send('Ciao Mondo'))
+
+app.listen(port, () => console.log(`Example app listening on port ${port}!!!!!!!!`))
